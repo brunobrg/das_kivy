1adb96fcf852ba5044152ad57bbc4204effad0a4
brunobrg <bbraganca1991@gmail.com>
2016-06-15 02:59:30 -0300

    adicionado botao de retorno, retirando views anteriores

--- controller/object.py
+++ controller/object.py
@@ -4,17 +4,20 @@ from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 from kivy.uix.widget import Widget
 from kivy.uix.stacklayout import StackLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from model.command import ObjectCommand
 
 presentation = Builder.load_file("view/object.kv")
 
-class Directory(Button):
+class Object(Button):
     obj = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super(Object, self).__init__(**kwargs)
 
     def click(self):
         app = App.get_running_app()
-        self.clear_widgets()
         OpenObject(app.session.user, self.obj).execute()
 
 
@@ -24,11 +27,13 @@ class DirectoryScreen(Screen):
 
     def update(self):
         app = App.get_running_app()
-        stack = StackLayout(orientation='lr-tb', spacing=[10,10])
         objs = app.session.directory.containing_objects
-        for _obj in objs:
-            stack.add_widget(Directory(obj=_obj, text=_obj.name, size_hint=(1/len(objs),1/len(objs))))
-        self.add_widget(stack)
+        for _obj in objs[len(self.ids.stack.ids):]:
+            self.ids.stack.add_widget(Object(obj=_obj, text=_obj.name, size_hint=(0.2,0.2)))
+
+    def destroy(self):
+        app = App.get_running_app()
+        app.root.remove_widget(self)
 
 class OpenObject(ObjectCommand):
 
@@ -38,7 +43,7 @@ class OpenObject(ObjectCommand):
     def run(self):
         app = App.get_running_app()
         app.session.directory = self.object
-        app.root.add_widget(DirectoryScreen(name='%s' % self.object.path))
+        app.root.add_widget(DirectoryScreen(name='%s' % self.object.path, id=self.object.path))
         app.root.current = self.object.path
 
     def success(self):
